fcc79822b55cae9493a14ed8cd51b1d5ae66310d
aletkm <81667024+aletkm@users.noreply.github.com>
2021-05-21 16:13:05 -0300

    Add files via upload

---
+++ data/GetAllUsers.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+function GetAllUsers() {
+    const [AllUsers, setAllUsers] = useState(null);
+
+    useEffect(() => {
+        // GET request using fetch inside useEffect React hook
+        fetch(`/api/graphql/gql_users`)
+            .then(response => response.json())
+            .then(data => setAllUsers(JSON.stringify(data)));
+
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+    }, []);
+
+    return (
+         AllUsers
+    );
+}
+
+export { GetAllUsers };
