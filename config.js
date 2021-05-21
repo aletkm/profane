728d08c079513e07014bd26071bc18b6d1b306ac
aletkm <81667024+aletkm@users.noreply.github.com>
2021-05-21 16:35:54 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const apiUrl = process.env.NODE_ENV === 'development' 
-    ? 'https://profane.vercel.app/api' // development api
-    : 'https://profane.vercel.app/api'; // production api
-
-export {
-    apiUrl
-};
